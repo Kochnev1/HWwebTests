3e5d482c27966d5785f0cb3116cb5ef186fd8627
Roman Kochnev <romaslog@icloud.com>
2023-09-28 22:11:17 +0500

    Tests passed

--- src/test/java/ru/netology/web/CallbackTest.java
+++ src/test/java/ru/netology/web/CallbackTest.java
@@ -28,7 +28,7 @@ class CallbackTest {
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
         driver = new ChromeDriver(options);
-        driver.get("http://localhost:9999");
+
     }
 
     @AfterEach
@@ -39,6 +39,7 @@ class CallbackTest {
 
     @Test
     void shouldTestSomething() {
+        driver.get("http://localhost:9999");
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Ветров Геннадий");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79090091919");
         driver.findElement(By.cssSelector("[data-test-id='agreement'] ")).click();
